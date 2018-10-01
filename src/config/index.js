84ebb91138b4305f73692fc34438187231371c1f
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2018-10-01 18:13:22 +0800

    feat: setup project structure

---
+++ src/config/index.js
@@ -0,0 +1,42 @@
+// 參數化變數
+
+export default {
+    HIGHEST_SCORE_STORAGE: 'HIGHEST_SCORE_STORAGE', // 儲存最高分
+    QUESTION_COUNT: 5, // 題目數量
+    SELECTION_COUNT: 30, // 有多少選項可以回答
+    MEMO_TIME: 15, // 有多少時間可以看圖記憶
+    ANSWER_TIME: 15,  // 有多少時間可以解題回答
+    iconNames: { 
+        MaterialCommunityIcons: [
+            "airplane",
+            "alarm-bell",
+            "cloud",
+            "asterisk",
+            "bandcamp",
+            "briefcase",
+            "bomb",
+            "brightness-1",
+            "brightness-2",
+            "camera-iris",
+            "cube",
+            "crown",
+            "duck",
+            "flash",
+            "emoticon-poop",
+            "ghost",
+            "github-face",
+            "hexagon",
+            "heart",
+            "music-circle",
+            "pizza"
+        ]
+    },
+    iconColors: [
+        "#F44336",
+        "#BA68C8",
+        "#2196F3",
+        "#607D8B",
+        "#8D6E63",
+        "#4CAF50"
+    ]
+}
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native';
+import { Router, Scene, Stack } from 'react-native-router-flux';
+import Home from './screens/homePage';
+import Memo from './screens/memoPage';
+import Exam from './screens/examPage';
+
+class Routes extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Routes;
